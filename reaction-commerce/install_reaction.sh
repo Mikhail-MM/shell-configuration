9842f86e14e699b535c410dbfc0a43b21c0c430d
Mikhail-MM <leakymee@bu.edu>
2019-12-13 21:25:23 -0800

    Update reaction-setup script to checkout old branch of reaction-platform to avoid missing 'reaction-identity' repo makefile issue

--- reaction-commerce/install_reaction.sh
+++ reaction-commerce/install_reaction.sh
@@ -1,3 +1,5 @@
 git clone git@github.com:reactioncommerce/reaction-platform.git ~/reaction-platform
-cd reaction-platform
+cd ~/reaction-platform
+# Avoid MAKE error from 3.0.0
+git checkout c991b88c3eaf04326a5a01c24b110eb10a613758
 make
